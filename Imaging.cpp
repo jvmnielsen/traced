c9fa390474db1100ba6ba03a3aefd51e542893a0
Jacob Vesti Moeslund Nielsen <jacob.vesti.moeslund@gmail.com>
2018-10-04 01:30:49 +0200

    Imaging functions

--- Imaging.cpp
+++ Imaging.cpp
@@ -1,44 +1,78 @@
-#include "ImageBuffer.h"
-
-ImageBuffer::ImageBuffer( const int channels, 
-                          const int screenWidth,
-                          const int screenHeight )
-    : m_channels( channels )
-    , m_screenWidth( screenWidth )
-    , m_screenHeight( screenHeight )
-    , m_counter(0)
+#include "Imaging.h"
+// ---------------------------------------------------------------------------
+Color::Color()
+    : red(0.0)
+    , green(0.0)
+    , blue(0.0)
 {
-    int screen_size = m_screenWidth * m_screenHeight * channels;
-    m_pixel_data.reserve( screen_size );
-
-    //Pixel white_pixel{ 255, 255, 255, 255 };
-
-    for (int i = 0; i < screen_size; ++i)
-    {
-        m_pixel_data.push_back( 255 );
-    }
-    //pixels = unsigned char[m_screenWidth * m_screenHeight * m_channels];
 }
-
-void color_clamp(Vec3f& color)
+// ---------------------------------------------------------------------------
+Color::Color(
+    const double _red,
+    const double _green,
+    const double _blue)
+    : red(_red)
+    , green(_green)
+    , blue(_blue)
+{ 
+}
+// ---------------------------------------------------------------------------
+void Color::Validate() const
 {
-    for (auto i = 0; i < 3; i++)
+    if ((red < 0.0) || (green < 0.0) || (blue < 0.0))
     {
-        if (color[i] > 255)
-            color[i] = 255;
-
-        if (color[i] < 0)
-            color[i] = 0;
+        throw ImagerException("Negative color values not allowed.");
     }
 }
-
-void ImageBuffer::add_pixel(Vec3f& pixel)
+// ---------------------------------------------------------------------------
+Color& Color::operator *= (const double factor)
 {
-    color_clamp(pixel);
-
-    for (int i = 0; i < 3; i++)
+    red   *= factor;
+    green *= factor;
+    blue  *= factor;
+    return *this;
+}
+// ---------------------------------------------------------------------------
+Color& Color::operator /= (const double factor)
+{
+    red   /= factor;
+    green /= factor;
+    blue  /= factor;
+    return *this;
+}
+// ---------------------------------------------------------------------------
+Color& Color::operator += (const Color& other)
+{
+    red   += other.red;
+    green += other.green;
+    blue  += other.blue;
+    return *this;
+}
+// ---------------------------------------------------------------------------
+Color& Color::operator -= (const Color& other)
+{
+    red   -= other.red;
+    green -= other.green;
+    blue  -= other.blue;
+    return *this;
+}
+// ---------------------------------------------------------------------------
+ImageBuffer::ImageBuffer(
+    const int _screenWidth, 
+    const int _screenHeight)
+    : screenWidth(_screenWidth)
+    , screenHeight(_screenHeight)
+{
+    storage.reserve(screenWidth * screenHeight); // reserve the total screen size for performance
+}
+// ---------------------------------------------------------------------------
+Color& ImageBuffer::PixelAt(size_t i, size_t j)
+{
+    if ((i < screenWidth) && (j < screenHeight))
     {
-        m_pixel_data[m_counter++] = (unsigned char)pixel[i];
+        return storage[(j * screenWidth) + i];
     }
-    m_pixel_data[m_counter++] = 255; // alpha
-} 
+
+    throw ImagerException("Pixel coordinate out of bounds.");
+}
+// ---------------------------------------------------------------------------
